b8b6df7af01169857aec9412a0d43b3dc223fab2
Stefano Pigozzi <me@steffo.eu>
2022-02-15 15:35:17 +0100

    :notebook_with_decorative_cover: Document more fields

--- src/structs/file_format.rs
+++ src/structs/file_format.rs
@@ -25,6 +25,7 @@ pub struct FileFormat {
     /// Pointers to the other sections of the file.
     pub pointers: FileSectionPointers,
 
+    /// Data for determining the FrameImportant property of tiles.
     pub tileframeimportant: TileFrameImportant,
 }
 
@@ -39,46 +40,57 @@ pub struct FileSectionPointers {
 }
 
 impl FileSectionPointers {
+    /// Byte offset from the start of the world save file to the "World Header" section.
     pub fn world_header(&self) -> u32 {
         self.contents[0]
     }
 
+    /// Byte offset from the start of the world save file to the "World Tiles" section.
     pub fn world_tiles(&self) -> u32 {
         self.contents[1]
     }
 
+    /// Byte offset from the start of the world save file to the "Chests" section.
     pub fn chests(&self) -> u32 {
         self.contents[2]
     }
 
+    /// Byte offset from the start of the world save file to the "Signs" section.
     pub fn signs(&self) -> u32 {
         self.contents[3]
     }
 
+    /// Byte offset from the start of the world save file to the "NPCs" section.
     pub fn npcs(&self) -> u32 {
         self.contents[4]
     }
 
+    /// Byte offset from the start of the world save file to the "Tile Entities" section.
     pub fn tile_entities(&self) -> u32 {
         self.contents[5]
     }
 
+    /// Byte offset from the start of the world save file to the "Pressure Plates" section.
     pub fn pressure_plates(&self) -> u32 {
         self.contents[6]
     }
 
+    /// Byte offset from the start of the world save file to the "Town Manager" section.
     pub fn town_manager(&self) -> u32 {
         self.contents[7]
     }
 
+    /// Byte offset from the start of the world save file to the "Bestiary" section.
     pub fn bestiary(&self) -> u32 {
         self.contents[8]
     }
 
+    /// Byte offset from the start of the world save file to the "Journey Powers" section.
     pub fn journey_powers(&self) -> u32 {
         self.contents[9]
     }
 
+    /// Byte offset from the start of the world save file to the "Footer" section.
     pub fn footer(&self) -> u32 {
         self.contents[10]
     }
